132b3048c03edbaf296a96d5713eea8556c873d5
DIVYA KUMARI <divya85390@gmail.com>
2026-05-06 10:15:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -38,3 +38,8 @@ function addTask() {
 
     document.getElementById("task").value = "";
 }
+async function loadData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const data = await res.json();
+  document.getElementById('api-content').innerText = data.title;
+}
